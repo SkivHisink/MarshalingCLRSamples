08e61ebef8ff233e82702c8ef390383ef4d68939
SkivHisink <49669372+SkivHisink@users.noreply.github.com>
2023-08-05 18:37:41 +0700

    Added assemblyinfo

---
+++ CLRSampleCpp/AssemblyInfo.cpp
@@ -0,0 +1,15 @@
+#include "AssemblyInfo.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"Title")] ;
+[assembly:AssemblyDescriptionAttribute(L"Description")] ;
+[assembly:AssemblyCompanyAttribute(L"Company")] ;
+[assembly:AssemblyProductAttribute(L"Product")] ;
+[assembly:AssemblyCopyrightAttribute(L"© Company. All rights reserved.")] ;
+[assembly:AssemblyTrademarkAttribute(L"Trademark")] ;
+[assembly:AssemblyVersionAttribute("1.0.0.0")] ;
